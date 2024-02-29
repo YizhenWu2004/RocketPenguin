dcdb880da768a47da9aafa63369d3cb150e3c5a3
lag288 <ligiraldo10@gmail.com>
2024-02-29 11:26:49 -0500

    Added Obstacle Classes from Physics Lab

---
+++ core/src/com/raccoon/mygame/GDXRoot.java
@@ -0,0 +1,97 @@
+/*
+ * GDXRoot.java
+ *
+ * This is the primary class file for running the game.  It is the "static main" of
+ * LibGDX.  In the first lab, we extended ApplicationAdapter.  In previous lab
+ * we extended Game.  This is because of a weird graphical artifact that we do not
+ * understand.  Transparencies (in 3D only) is failing when we use ApplicationAdapter. 
+ * There must be some undocumented OpenGL code in setScreen.
+ *
+ * Author: Walker M. White
+ * Based on original PhysicsDemo Lab by Don Holden, 2007
+ * Updated asset version, 2/6/2021
+ */
+ package com.raccoon.mygame;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.raccoon.mygame.*;
+import util.ScreenListener;
+
+/**
+ * Root class for a LibGDX.  
+ * 
+ * This class is technically not the ROOT CLASS. Each platform has another class above
+ * this (e.g. PC games use DesktopLauncher) which serves as the true root.  However, 
+ * those classes are unique to each platform, while this class is the same across all 
+ * plaforms. In addition, this functions as the root class all intents and purposes, 
+ * and you would draw it as a root class in an architecture specification.  
+ */
+public class GDXRoot extends Game implements ScreenListener {
+	/** Drawing context to display graphics (VIEW CLASS) */
+	private GameCanvas canvas;
+	/** Player mode for the the game proper (CONTROLLER CLASS) */
+	private int current;
+	
+	/**
+	 * Creates a new game from the configuration settings.
+	 *
+	 * This method configures the asset manager, but does not load any assets
+	 * or assign any screen.
+	 */
+	public GDXRoot() { }
+
+	/** 
+	 * Called when the Application is first created.
+	 * 
+	 * This is method immediately loads assets for the loading screen, and prepares
+	 * the asynchronous loader for all other assets.
+	 */
+	public void create() {
+		canvas  = new GameCanvas();
+	}
+
+	/** 
+	 * Called when the Application is destroyed. 
+	 *
+	 * This is preceded by a call to pause().
+	 */
+	public void dispose() {
+		// Call dispose on our children
+		setScreen(null);
+
+		canvas.dispose();
+		canvas = null;
+
+		super.dispose();
+	}
+	
+	/**
+	 * Called when the Application is resized. 
+	 *
+	 * This can happen at any point during a non-paused state but will never happen 
+	 * before a call to create().
+	 *
+	 * @param width  The new width in pixels
+	 * @param height The new height in pixels
+	 */
+	public void resize(int width, int height) {
+		canvas.resize();
+		super.resize(width,height);
+	}
+
+	/**
+	 * The given screen has made a request to exit its player mode.
+	 *
+	 * The value exitCode can be used to implement menu options.
+	 *
+	 * @param screen   The screen requesting to exit
+	 * @param exitCode The state of the screen upon exit
+	 */
+	public void exitScreen(Screen screen, int exitCode) {
+		// We quit the main application
+		Gdx.app.exit();
+	}
+
+}
