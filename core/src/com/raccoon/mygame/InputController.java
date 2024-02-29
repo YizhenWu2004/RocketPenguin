1e274fe13bc6d43ca1e7c6cab1edf059f80d23a6
Yizhen Wu <yw2343@cornell.edu>
2024-02-29 17:02:28 -0500

    updated InputController

--- core/src/com/raccoon/mygame/InputController.java
+++ core/src/com/raccoon/mygame/InputController.java
@@ -2,20 +2,19 @@ package com.raccoon.mygame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.utils.*;
 import java.util.*;
 import com.badlogic.gdx.controllers.Controller;
 
-public class InputController implements InputProcessor {
+public class InputController{
     /**for x and y movement, 1 represent up/right and -1 represent down/left
-     * for scroll, positive represent scroll up and negative scroll down (may be the opposite, spec unclear).
-     *  also, the float scroll might not be 1 or -1, so might need to adjust later in code.*/
+     * for scroll, -1 represent left and 1 represent right scroll direction*/
     private float y_movement;
     private float x_movement;
-    private boolean left_click;
-    private boolean right_click;
     private float scroll;
+    private boolean interaction;
 
     public float getYMovement(){
         return y_movement;
@@ -24,74 +23,18 @@ public class InputController implements InputProcessor {
     public float getXMovement(){
         return x_movement;
     }
-    public boolean getLeftClick(){
-        return left_click;
-    }
-    public boolean getRightClick(){
-        return right_click;
-    }
     public float getScroll(){
         return scroll;
     }
 
-    @Override
-    public boolean keyDown(int keycode) {
-        // Handle key press events
-        return false;
-    }
+    public boolean getInteraction(){return interaction;}
 
-    @Override
-    public boolean keyTyped(char keycode) {
-        // Handle key press events
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        // Handle key release events
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        // Handle touch/mouse down events
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        // Handle touch/mouse up events
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        // Handle mouse wheel scrolling
-        scroll= amountY;
-        return false;
-    }
 
     public InputController(){
         y_movement = 0;
         x_movement = 0;
-        left_click = false;
-        right_click = false;
         scroll = 0;
+        interaction = false;
     }
     public void readInput(){
 
@@ -99,17 +42,30 @@ public class InputController implements InputProcessor {
             y_movement = 1;
         } else if (Gdx.input.isKeyPressed(Input.Keys.S)){
             y_movement = -1;
+        } else {
+            y_movement = 0;
         }
+
         if (Gdx.input.isKeyPressed(Input.Keys.D)){
             x_movement = 1;
         } else if (Gdx.input.isKeyPressed(Input.Keys.A)){
             x_movement = -1;
+        } else {
+            x_movement = 0;
         }
 
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-            left_click = true;
-        } else if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
-            right_click = true;
+        if (Gdx.input.isKeyPressed(Keys.LEFT)){
+            scroll = -1;
+        } else if (Gdx.input.isKeyPressed(Keys.RIGHT)){
+            scroll = 1;
+        } else {
+            scroll = 0;
+        }
+
+        if (Gdx.input.isKeyPressed(Keys.E)){
+            interaction = true;
+        } else {
+            interaction = false;
         }
 
     }
