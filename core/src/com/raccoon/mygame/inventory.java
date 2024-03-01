c8745f34980236acc0979757291072324acf73ad
Elmo Tang <67039163+an-emo-inchworm@users.noreply.github.com>
2024-03-01 13:45:23 -0800

    made the array a gameobject array

--- core/src/com/raccoon/mygame/inventory.java
+++ core/src/com/raccoon/mygame/inventory.java
@@ -1,14 +1,16 @@
 package com.raccoon.mygame;
 
+import com.raccoon.mygame.objects.GameObject;
 import com.raccoon.mygame.obstacle.BoxObstacle;
+import com.raccoon.mygame.view.GameCanvas;
 
 public class inventory {
-    private BoxObstacle[] items;
+    private GameObject[] items;
     private int index;
     private boolean[] filled;
 
     public inventory(){
-        items = new BoxObstacle[5];
+        items = new GameObject[5];
         index = 0;
         filled = new boolean[5];
         for(int i = 0; i < filled.length; i++){
@@ -19,7 +21,7 @@ public class inventory {
     public void draw(GameCanvas canvas){
         for(int i = 0; i < items.length; i++){
             if(filled[i] = false){
-                items[i].drawDebug(canvas);
+                canvas.draw(items[i].getTexture(), 100 * (i+1), 100);
             }
         }
     }
