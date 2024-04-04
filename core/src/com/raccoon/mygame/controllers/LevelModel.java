469d0e1db4ef8d0f3298695e8bf72bb5547be635
LegendFinder <legendfinder748@gmail.com>
2024-04-04 16:08:05 -0400

    WIP

---
+++ core/src/com/raccoon/mygame/controllers/LevelModel.java
@@ -0,0 +1,10 @@
+package com.raccoon.mygame.controllers;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class LevelModel {
+    public LevelModel(JsonValue levelData) {
+
+    }
+}
