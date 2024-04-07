4542813e1cfb68ec96f1f0119337b5028db6e6e4
Yizhen Wu <yw2343@cornell.edu>
2024-04-07 00:10:46 -0400

    ummm retry

--- core/src/com/raccoon/mygame/controllers/Worldtimer.java
+++ core/src/com/raccoon/mygame/controllers/Worldtimer.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Timer;
 import com.raccoon.mygame.view.GameCanvas;
@@ -13,6 +14,9 @@ public class Worldtimer extends ApplicationAdapter {
     private int countdownSeconds;
     private GameCanvas gameCanvas;
     public boolean action_round;
+    BitmapFont f = new BitmapFont();
+    GlyphLayout layout = new GlyphLayout(f, "");
+
 
     public Worldtimer(int count, GameCanvas canvas){
         countdownSeconds = count;
@@ -34,9 +38,27 @@ public class Worldtimer extends ApplicationAdapter {
             }
         }, 1, 1);
     }
-    public void draw(int x, int y){
-        gameCanvas.drawText(Integer.toString(countdownSeconds), new BitmapFont(), x,y);
+//    public void draw(int x, int y){
+//        gameCanvas.drawText(Integer.toString(countdownSeconds), f, x,y);
+//    }
+
+    public void draw(int x,int y) {
+        int mins = 0;
+        int secs = 0;
+        if (countdownSeconds > 60) {
+            mins = countdownSeconds / 60;
+            secs = countdownSeconds % 60;
+        }
+        if (secs < 10) {
+            gameCanvas.drawText(Integer.toString(mins) + ":0" + Integer.toString(secs),
+                    f, 20, 700, 2, 2,layout);
+        } else {
+            gameCanvas.drawText(Integer.toString(mins) + ":" + Integer.toString(secs),
+                    f, 20, 700,2,2,layout);
+        }
     }
+
+
 
     public int getTime(){
         return countdownSeconds;
