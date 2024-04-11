2d2879ada0528f66ce295fc51d81eeb8db83b5d1
LegendFinder <legendfinder748@gmail.com>
2024-04-11 02:03:02 -0400

    progress on level model

--- core/src/com/raccoon/mygame/controllers/LevelLoader.java
+++ core/src/com/raccoon/mygame/controllers/LevelLoader.java
@@ -1,24 +1,21 @@
 package com.raccoon.mygame.controllers;
 
-import com.badlogic.gdx.utils.Json;
 import com.raccoon.mygame.assets.AssetDirectory;
 import com.badlogic.gdx.utils.JsonValue;
-import com.raccoon.mygame.controllers.LevelModel;
 import com.raccoon.mygame.view.GameCanvas;
 
 import java.util.ArrayList;
-import java.util.logging.Level;
 
 public class LevelLoader {
     private JsonValue restaurantJson;
     private JsonValue levelJson;
-    private ArrayList<LevelModel> levels;
+    private ArrayList<LevelModelAlt> levels;
 
     public LevelLoader(AssetDirectory directory, GameCanvas canvas) {
         gatherAssets(directory);
         levels = new ArrayList<>();
         for (int i = 0; i < levelJson.size; i++) {
-            levels.add(new LevelModel(levelJson.get(i), restaurantJson, canvas));
+            levels.add(new LevelModelAlt(levelJson.get(i), restaurantJson, canvas));
         }
     }
 
@@ -27,5 +24,5 @@ public class LevelLoader {
         levelJson = directory.getEntry("levels", JsonValue.class).get("levels");
     }
 
-    public ArrayList<LevelModel> getLevels() { return levels; }
+    public ArrayList<LevelModelAlt> getLevels() { return levels; }
 }
--- core/src/com/raccoon/mygame/controllers/LevelModel.java
+++ core/src/com/raccoon/mygame/controllers/LevelModel.java
@@ -4,77 +4,99 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.JsonValue;
+import com.raccoon.mygame.models.Guard;
 import com.raccoon.mygame.objects.Ingredient;
 import com.raccoon.mygame.objects.NormalObstacle;
-import com.raccoon.mygame.models.Guard;
-import com.raccoon.mygame.objects.TableObstacle;
-import com.raccoon.mygame.objects.CookingStationObject;
-import com.raccoon.mygame.objects.Ingredient;
 import com.raccoon.mygame.view.GameCanvas;
-import com.raccoon.mygame.enums.enums;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
 
 public class LevelModel {
-    private World restaurantWorld;
     private World storeWorld;
     private GameCanvas canvas;
-    private Array<NormalObstacle> restaurantObjects = new Array<>();
-    private Array<TableObstacle> restaurantTables = new Array<>();
-    private Array<CookingStationObject> restaurantCounters = new Array<>();
     private Array<NormalObstacle> storeObjects = new Array<>();
-    private Array<Ingredient> ingredients = new Array<>();
+    private Array<NormalObstacle> ingredients = new Array<>();
     private Array<Guard> guards = new Array<>();
-
-    private void addTable(float x, float y, boolean flip) {
-        TableObstacle t = new TableObstacle(x, y, 2.5f, 2.5f, (flip ? -0.25f : 0.25f), 0.25f, -50f, 50f,
-                new Texture("table.png"), restaurantWorld, canvas);
-        restaurantObjects.add(t);
-        restaurantTables.add(t);
-    }
-
-    private void addWallBump(float x, float y) {
-        NormalObstacle obstacle = new NormalObstacle(x, y, 2.5f, 5f, 1f, 1f, 0f, 0f,
-                new Texture("wallbump.png"), restaurantWorld, canvas);
-        restaurantObjects.add(obstacle);
-    }
+    private Array<Array<Array<Float>>> guardNodes = new Array<>();
+    private TiledMap tiledMap;
+    private MapLayer storeObjectsLayer;
+    private MapLayer ingredientsLayer;
+    private MapLayer guardsLayer;
+    private MapLayer guardNodesLayer;
 
     private void addShelfHorizontal(float x, float y) {
         NormalObstacle obstacle = new NormalObstacle(x, y, 5.25f, 1f, 0.25f, 0.25f, 0f, -100f,
-                new Texture("groceryshelfhorizontal.png"), storeWorld, canvas);
+                new Texture("720/groceryshelfhorizontal.png"), storeWorld, canvas);
         storeObjects.add(obstacle);
     }
 
     private void addShelfVertical(float x, float y) {
         NormalObstacle obstacle = new NormalObstacle(x, y, 1f, 6f, 0.3f, 0.3f, 0f, 0f,
-                new Texture("groceryshelfvertical.png"), storeWorld, canvas);
+                new Texture("720/groceryshelfvertical.png"), storeWorld, canvas);
         storeObjects.add(obstacle);
     }
 
     private void addFruitCrate(float x, float y, Ingredient ingredient) {
         NormalObstacle obstacle = new NormalObstacle(x, y, 2f, 1f, 0.4f, 0.4f, 0f, 0f,
-                new Texture(ingredient.type + ".png"), storeWorld, canvas, ingredient);
-        storeObjects.add(obstacle);
+                new Texture("720/" + ingredient.type + ".png"), storeWorld, canvas, ingredient);
+        ingredients.add(obstacle);
     }
 
-    public LevelModel(JsonValue levelData, JsonValue restaurantData, GameCanvas canvas) {
-        restaurantWorld = new World(new Vector2(0, 0), false);
+    public LevelModel(String tmxFile, GameCanvas canvas) {
         storeWorld = new World(new Vector2(0, 0), false);
         this.canvas = canvas;
+        tiledMap = new TmxMapLoader().load(tmxFile + ".tmx");
+        storeObjectsLayer = tiledMap.getLayers().get("Obstacles");
+        ingredientsLayer = tiledMap.getLayers().get("Ingredients");
+        guardsLayer = tiledMap.getLayers().get("Guards");
+        guardNodesLayer = tiledMap.getLayers().get("GuardNodes");
     }
 
-    private void gatherRestaurantObstacles(JsonValue data) {
-        for (JsonValue obstacle : data) {
-            switch (obstacle.getString("type")) {
-                case "counterleft":
-                    /*CookingStationObject c = new CookingStationObject(28f, 15f, 3.25f, 4f, 0.25f, 0.25f, 0f, 0f,
-                            new Texture("counterleft.png"), restaurantWorld, canvas, player, 1);
-                    restaurantObjects.add(c);
-                    restaurantCounters.add(c);*/
+    private Array<Float> createNode(MapObject n) {
+        Array<Float> node = new Array<Float>();
+        node.add((float)n.getProperties().get("X"));
+        node.add((float)n.getProperties().get("Y"));
+        node.add((float)n.getProperties().get("Sleep"));
+        node.add((float)n.getProperties().get("Time"));
+        return node;
+    }
+
+    private void processObjects() {
+        for (MapObject o : storeObjectsLayer.getObjects()) {
+            switch (o.getName()) {
+                case "HorizShelf":
+                    break;
+                case "VertShelf":
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    private void processIngredients() {
+        for (MapObject i : ingredientsLayer.getObjects()) {
+            Ingredient ing = new Ingredient(i.getName(), new Texture("720/" + i.getName() + ".png"), -1);
+            addFruitCrate((float)i.getProperties().get("X"), (float)i.getProperties().get("Y"), ing);
+        }
+    }
+
+    private void processGuards() {
+        for (MapObject g : guardsLayer.getObjects()) {
+            int idx = (int)g.getProperties().get("GuardNum");
+            Array<Array<Float>> nodes = new Array<>();
+            nodes.add(createNode(g));
+            guardNodes.set(idx, nodes);
+        }
+        for (MapObject n : guardNodesLayer.getObjects()) {
+            int gIdx = (int)n.getProperties().get("GuardNum");
+            int nIdx = (int)n.getProperties().get("NodeNum");
+            guardNodes.get(gIdx).set(nIdx, createNode(n));
+        }
+        //CREATE ACTUAL GUARD OBJECT
     }
 }
