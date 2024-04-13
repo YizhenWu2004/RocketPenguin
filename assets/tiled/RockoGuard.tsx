ccc7430f97ff09876318872f9e218c4fa29a20d5
LegendFinder <legendfinder748@gmail.com>
2024-04-13 19:28:22 -0400

    Made level model not crash

---
+++ assets/tiled/RockoGuard.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RockoGuard" tilewidth="204" tileheight="263" tilecount="1" columns="1">
+ <image source="Guard.png" width="204" height="263"/>
+</tileset>
