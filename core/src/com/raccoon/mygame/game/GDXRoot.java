9672ad67f905b9783fd367a925c9d6a3e3e16a62
Olivia Jiang <jiangxolivia@gmail.com>
2024-04-25 11:05:51 -0400

    commented out debug

--- core/src/com/raccoon/mygame/game/GDXRoot.java
+++ core/src/com/raccoon/mygame/game/GDXRoot.java
@@ -398,12 +398,12 @@ public class GDXRoot extends Game implements ScreenListener {
     }
 
     public void drawDebug() {
-        canvas.beginDebug();
-        if (current == 1) {
-            store.debug();
-        } else if(current == 0){
-            restaurant.debug();
-        }
-        canvas.endDebug();
+//        canvas.beginDebug();
+//        if (current == 1) {
+//            store.debug();
+//        } else if(current == 0){
+//            restaurant.debug();
+//        }
+//        canvas.endDebug();
     }
 }
