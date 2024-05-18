b74ad59bc23ca13b5d320e062cbd89918ef5a893
Yizhen Wu <yw2343@cornell.edu>
2024-05-17 21:07:47 -0400

    added booklet ui

--- core/src/com/raccoon/mygame/game/GDXRoot.java
+++ core/src/com/raccoon/mygame/game/GDXRoot.java
@@ -619,7 +619,7 @@ public class GDXRoot extends Game implements ScreenListener {
         } else if (current == 1) {
             store.draw();
             if(timeoutTimer > 0){
-                canvas.drawTextCentered("The day is over",new BitmapFont(),0);
+                //canvas.drawTextCentered("The day is over",new BitmapFont(),0);
             }
         } else if (current == 2){
 //            result.setStarReq(this.star_req);
